c87a78fe63e85e213c3ca19475c853a261d48b10
maevadesfours <maevadesfours@localhost>
2023-04-25 17:41:13 +0200

    22th commit

--- src/main/java/com/mycompany/mavenproject2/OrderPart.java
+++ src/main/java/com/mycompany/mavenproject2/OrderPart.java
@@ -10,21 +10,28 @@ import org.json.simple.JSONObject;
  *
  * @author maevadesfours
  */
+
+//création d'un objet JSON 
+//on le défini par son identifiant et sa quantité 
+//il permet lors de la lecture du fichier que l'identifiant reconnu soit associé à sa quantité
+// ce sera un JSONObject nommé OrderPart
+
 public class OrderPart {
     Long id;
     Long qty;
+    //on utilise des long et non des int
     
     public OrderPart(JSONObject obj){
-        this.id = (Long)obj.get("id");
-        this.qty = (Long)obj.get("qty");
-               
-}
-
+        this.id = (Long)obj.get("id");//on récupère l'dentifiant dans le fichier JSON
+        this.qty = (Long)obj.get("qty");//on récupère la quantité associée
+    }
     @Override
     public String toString() {
         return "{id=" + id + ", qty=" + qty + '}';
     }
-
+    //on retourne l'id et sa qt
+    
+    // on génère ensuite des guetters et setters
     public Long getId() {
         return id;
     }
